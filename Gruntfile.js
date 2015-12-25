29173d49d154dc06e98ec21645d1652510e0b423
Steffan Long <Steffwiz@me.com>
2015-12-25 15:00:11 -0800

    enable api server again, fix missing html partials

--- Gruntfile.js
+++ Gruntfile.js
@@ -460,7 +460,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       trialsReportApp: {
         cwd: '<%= yeoman.app %>',
-        src: '{,*/}*.html',
+        src: '**/*.html',
         dest: '.tmp/templates.js',
         options: {
           usemin: 'app.js',
