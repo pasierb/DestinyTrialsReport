ddddb8ed8ab0069ecd6922a7075bc1844af175fe
Steffan Long <Steffwiz@me.com>
2015-12-30 19:04:16 -0800

    get rid of annoying images in root

--- Gruntfile.js
+++ Gruntfile.js
@@ -391,6 +391,11 @@ module.exports = function (grunt) {
           cwd: '<%= yeoman.app %>/assets/img',
           src: '{,*/}*.{png,jpg,jpeg,gif}',
           dest: '<%= yeoman.dist %>/assets/img'
+        }, {
+          expand: true,
+          cwd: '<%= yeoman.app %>/assets/img/icons',
+          src: '*.{png,ico}',
+          dest: '<%= yeoman.dist %>'
         }]
       }
     },
