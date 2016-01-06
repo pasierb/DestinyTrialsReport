85bdecfc67c9ec3db4eea9347e10d33d3b51b6f5
v-fedorov <vfedorov@netflexity.com>
2016-01-06 17:06:31 -0500

    update adsense

--- app/shared/directives/adsense.directive.js
+++ app/shared/directives/adsense.directive.js
@@ -8,8 +8,8 @@ angular.module('trialsReportApp')
       template: [
         '<ins class="adsbygoogle"' +
              'style="display:block"' +
-             'data-ad-client="ca-pub-5549262666967585"' +
-             'data-ad-slot="7148544984"' +
+             'data-ad-client="ca-pub-7408805581120581"' +
+             'data-ad-slot="5173326955"' +
              'data-ad-format="auto">' +
         '</ins>'
       ].join(''),
