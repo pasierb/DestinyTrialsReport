b59b4badda72d2be3dee942dd6f35c4b5e1155c0
v-fedorov <vfedorov@netflexity.com>
2016-01-15 17:26:42 -0500

    cut ad in half

--- app/shared/directives/adsense.directive.js
+++ app/shared/directives/adsense.directive.js
@@ -10,11 +10,27 @@ angular.module('trialsReportApp')
              'style="display:block"' +
              'data-ad-client="ca-pub-7408805581120581"' +
              'data-ad-slot="1127916954"' +
-             'data-ad-format="rectangle">' +
+             'data-ad-format="auto">' +
         '</ins>'
       ].join(''),
       controller: function () {
         (adsbygoogle = window.adsbygoogle || []).push({});
       }
-    };
-});
+    }
+  })
+  .directive('adsenseSmall', function() {
+    return {
+      restrict: 'A',
+      replace: true,
+      template: [
+        '<ins class="adsbygoogle"' +
+        'style="display:inline-block;width:320px;height:50px"' +
+        'data-ad-client="ca-pub-7408805581120581"' +
+        'data-ad-slot="7815945353">' +
+        '</ins>'
+      ].join(''),
+      controller: function () {
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      }
+    }
+  });
