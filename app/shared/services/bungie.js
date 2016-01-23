06c1d4a7314e53b3db3f4603abff8f622e6c3ef4
v-fedorov <vfedorov@netflexity.com>
2016-01-23 02:15:22 -0500

    thank you

--- app/shared/services/bungie.js
+++ app/shared/services/bungie.js
@@ -8,6 +8,7 @@ app.service('bungie', [
 
   function ($http, util) {
     return new function () {
+      var BASE_URL = '//proxy.destinytrialsreport.com/Platform/Destiny/';
       var ENDPOINTS = {
         searchForPlayer: 'SearchDestinyPlayer/{platform}/{name}/',
         account: '{platform}/Account/{membershipId}/',
@@ -74,8 +75,6 @@ app.service('bungie', [
       };
 
       this.get = function(endpoint, tokens) {
-        var domain = Math.round(Math.random()) === 1 ? 'guardian.gg' : 'destinytrialsreport.com';
-        var BASE_URL = "//proxy." + domain + "/Platform/Destiny/";
         return $http.get(BASE_URL + util.buildUrl(endpoint, tokens));
       };
     };
