3b35f018340a50767840d3c1e740be669e0a07e4
v-fedorov <vfedorov@netflexity.com>
2016-05-27 10:36:55 -0400

    remove map hack again

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -248,8 +248,8 @@
     function setCurrentMap(id, week) {
       $scope.currentMapId = id;
       $scope.currentMap = DestinyCrucibleMapDefinition[id];
-      // $scope.loadMapInfo(week);
-      $scope.loadMapInfo(29);
+      $scope.loadMapInfo(week);
+      // $scope.loadMapInfo(29);
     }
 
     function getMapFromStorage() {
