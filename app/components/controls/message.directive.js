6c9fcefaf3b9c63496be1137bd3b2158dcff39bb
Mark Montvai <mark.montvai@gmail.com>
2017-01-06 18:39:17 +0100

    Message component was giving a console error, so added the directive for now

---
+++ app/components/controls/message.directive.js
@@ -0,0 +1,29 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('trialsReportApp')
+    .directive('message', message);
+
+  function message() {
+    return {
+      restrict: 'A',
+      scope: {
+        message: '=message',
+        messageType: '=messageType', // error, warning or info for now
+        messageId: '=messageId'
+      },
+      link: function ($scope) {
+        $scope.messageType = $scope.messageType || 'info';
+
+        $scope.isHidden = sessionStorage.getItem('hideMessage' + $scope.messageId);
+
+        $scope.hideMessage = function () {
+          sessionStorage.setItem('hideMessage' + $scope.messageId, true);
+          $scope.isHidden = true;
+        };
+      },
+      templateUrl: 'components/controls/message.template.html'
+    };
+  };
+}());
